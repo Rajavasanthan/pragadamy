751c373dcc7f1685f25a32a5066bed0166308827
rajavasanthan <rajavasanthan@gmail.com>
2019-10-23 22:39:36 +0530

    async

--- TS/car.ts
+++ TS/car.ts
@@ -17,6 +17,14 @@ export class Car extends Vehicle implements CarConfig{
     private brand;
     color;
 
+    get brandName(){
+        return this.brand;
+    }
+
+    set brandName(name){
+        this.brand = name;
+    }
+
     constructor(){
         super();
     }
